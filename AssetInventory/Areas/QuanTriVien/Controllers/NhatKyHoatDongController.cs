92d37082fc7f548caf6710e1d2b16cd80cd5da38
quejo_1101 <ducln339@gmail.com>
2023-02-03 13:04:41 +0700

    Add project files.

---
+++ AssetInventory/Areas/QuanTriVien/Controllers/NhatKyHoatDongController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AssetInventory.Models;
+
+namespace AssetInventory.Areas.QuanTriVien.Controllers
+{
+    public class NhatKyHoatDongController : Controller
+    {
+        AIDataContext db = new AIDataContext();
+
+        // GET: QuanTriVien/NhatKyHoatDong
+        public ActionResult Index()
+        {
+            if (Session["Admin"] == null || Session["Admin"].ToString() == "")
+            {
+                return RedirectToAction("DangNhap", "TrangChu");
+            }
+            else
+            {
+                return View();
+
+            }
+        }
+
+
+        [HttpGet]
+        public JsonResult Select_NKHD()
+        {
+            var get_data = from s in db.NhatKyHoatDongs.OrderByDescending(a => a.NgayHoatDong)
+                           select new { s.MaNKHD, s.TenDangNhap, s.HoatDong, s.ChiTietHoatDong, s.GhiChu, s.NgayHoatDong};
+            return Json(new { data = get_data }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AssetInventory/Areas/QuanTriVien/Controllers/TrangChuController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AssetInventory.Models;
+
+namespace AssetInventory.Areas.QuanTriVien.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        // GET: QuanTriVien/TrangChu
+        AIDataContext db = new AIDataContext();
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if (Session["Admin"] == null || Session["Admin"].ToString() == "")
+            {
+                return RedirectToAction("DangNhap", "TrangChu");
+            }else
+            {
+                return View();
+
+            }
+        }
+
+        [HttpGet]
+        public ActionResult DangNhap()
+        {
+            if (Session["Admin"] == null || Session["Admin"].ToString() == "")
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "TrangChu");
+
+            }
+            
+        }
+
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            var sTDN = collection["username"];
+            var sMK = collection["password"];
+            if (String.IsNullOrEmpty(sTDN))
+            {
+                ViewBag.ThongBao = "Chưa nhập tên đăng nhập kìa.";
+            }
+            else if (String.IsNullOrEmpty(sMK))
+            {
+                ViewBag.ThongBao = "Phải nhập mật khẩu.";
+            }
+            else
+            {
+
+                NguoiDung kh = db.NguoiDungs.SingleOrDefault(n => n.TenDangNhap == sTDN && n.MatKhau == sMK);
+                if (kh != null)
+                {
+                    Session["Admin"] = kh;
+
+                    NhatKyHoatDong nkhd = new NhatKyHoatDong();
+                    nkhd.TenDangNhap = kh.TenDangNhap;
+                    nkhd.HoatDong = "Đăng nhập";
+                    nkhd.ChiTietHoatDong = kh.TenDangNhap + " đăng nhập vào hệ thống thành công";
+                    nkhd.NgayHoatDong = DateTime.Now;
+                    db.NhatKyHoatDongs.InsertOnSubmit(nkhd);
+                    db.SubmitChanges();
+
+                    return RedirectToAction("Index", "TrangChu");
+                }
+                else
+                {
+                    ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu hông đúng.";
+                }
+            }
+
+            return View();
+        }
+
+
+
+        public ActionResult DangXuat()
+        {
+            FormsAuthentication.SignOut();
+            //Session.Abandon();
+            Session.Remove("Admin");
+            return RedirectToAction("DangNhap", "TrangChu");
+        }
+
+
+    }
+}
